4762cd168fd2f0ea6b4f8f5c4c007f5900e7106b
guilhermefaj <figueiredoguiu@gmail.com>
2023-03-08 11:15:27 -0300

    Test: Função map não funciona corretamente na SessionsPage

--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 
-export default function SessionsPage() {
+export default function SessionsPage({ movieSession }) {
+    const { id, title, posterURL, days } = movieSession;
 
     return (
         <PageContainer>
             Selecione o horário
+
             <div>
                 <SessionContainer>
                     Sexta - 03/03/2023
@@ -13,23 +15,19 @@ export default function SessionsPage() {
                         <button>15:00</button>
                     </ButtonsContainer>
                 </SessionContainer>
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
             </div>
+
+            {days.map((filme) => (
+                <div>
+                    <SessionContainer>
+                        {filme.weekday} - {filme.date}
+                        <ButtonsContainer>
+                            <button>{filme.showtimes[0].name}</button>
+                            <button>{filme.showtimes[1].name}</button>
+                        </ButtonsContainer>
+                    </SessionContainer>
+                </div>
+            ))}
 
             <FooterContainer>
                 <div>
